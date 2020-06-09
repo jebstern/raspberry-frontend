5c9981f0dde091f4fc1bce0ae363b4011627af43
Jesper Skand <jesper.skand@gmail.com>
2020-06-09 12:41:20 +0300

    add prod url to api service

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
-const API_URL = 'http://192.168.10.38:3100/ruuvitag'
-
-
+const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'http://raspberrypi:3100//ruuvitag'
+    : 'http://192.168.10.38:3100/ruuvitag'
 
 export async function tags(): Promise<any> {
   return await axios.get(`${API_URL}/tags`).then((res) => res.data)
 }
-
